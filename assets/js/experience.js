ac29576e966de42749f68c11c0c80f9ba757443a
Chetan Mahawar <72248044+chetanmhwr1111@users.noreply.github.com>
2026-05-24 21:48:57 +0530

    Update experience.js

--- assets/js/experience.js
+++ assets/js/experience.js
@@ -11,26 +11,29 @@ const exp = [
     time: "(Nov, 2023 to Present)",
     desp: "<li>Physics driven simulation enthusiast with the apt for virtualization of real engineering problems</li><li>Expertise in Mechanical fundamentals, FEA simulations, analytical validation, and result interpretation</li><li>Training & mentorship in Ansys Mechanical covering Non-linear, Thermal, Multiphysics, Composite analysis & Automation etc</li>",
   },
+
   {
     title: "Instructor",
     cardImage: "assets/images/experience-page/IGC_logo.png",
     place: "Freelancer",
     time: "(Nov, 2025 to Present)",
-    desp: "<li>Virtually teaching engineering students for GATE Mechanical Engineering courses</li><li>Expertise in Mechanical Engg fundamentals- SOM, Machine Design, Engineering Mechanics, Vibrations</li>
+    desp: "<li>Virtually teaching engineering students for GATE Mechanical Engineering courses</li><li>Expertise in Mechanical Engg fundamentals- SOM, Machine Design, Engineering Mechanics, Vibrations</li>",
   },
+
   {
     title: "Quality Engineer, Scientist C",
     cardImage: "assets/images/experience-page/ISRO_logo.png",
     place: "Indian Space Research Organisation",
     time: "(Dec, 2021 - Oct, 2023)",
-    desp: "<li>Involved in the Quality control of Design to Realisation of Fluid Control Components in all Earth Storable Stages of Launch vehicles of India.</li>","<li>Actively contributed for Chandrayaan 3 mission & Gaganyaan Unmanned mission<li>",
-  },  
+    desp: "<li>Involved in the Quality control of Design to Realisation of Fluid Control Components in all Earth Storable Stages of Launch vehicles of India.</li><li>Actively contributed for Chandrayaan 3 mission & Gaganyaan Unmanned mission</li>",
+  },
+
   {
     title: "Research Intern",
     cardImage: "assets/images/experience-page/Res_intern.jpg",
     place: "IIT, Bombay",
     time: "(Dec, 19 - Jan, 20)",
-    desp: "<li>First acquaintance of Numerical modelling of Geometrically Non-linear and Elastically linear beams in Woven composites<li></li>Reviewed existing, in-house, Numerical model of Straight and Wavy (Taffeta) composite woven fabrics in MATLAB<li>Implemented and documented various numerical tests of validation for the first list of FAQs and HTML files</li>",
+    desp: "<li>First acquaintance of Numerical modelling of Geometrically Non-linear and Elastically linear beams in Woven composites</li><li>Reviewed existing, in-house, Numerical model of Straight and Wavy (Taffeta) composite woven fabrics in MATLAB</li><li>Implemented and documented various numerical tests of validation</li>",
   },
 ];
 
